cf2fabba3aa9663c9e33b187015326483609b6ce
王智民 <183138816@qq.com>
2018-08-09 23:52:02 +0800

    modify db config

--- docker_db/create_db.sh
+++ docker_db/create_db.sh
@@ -1,9 +1,9 @@
 
 docker ps -a 
 
-docker stop user_center_db
+docker stop order_db
 
-docker rm user_center_db
+docker rm order_db
 
-docker run -d -p 254:3306 --restart=always --network ota_network --name user_center_db -v /etc/localtime:/etc/localtime -v $PWD/docker_db/sql:/docker-entrypoint-initdb.d -e TZ="Asia/Shanghai" -e MYSQL_DATABASE="user_center" -e MYSQL_ROOT_PASSWORD="123"  mysql --init-connect='SET NAMES utf8' --character-set-server=utf8 --collation-server=utf8_general_ci --explicit_defaults_for_timestamp=1
+docker run -d -p 254:3306 --restart=always --network order_network --name order_db -v $PWD/docker_db/sql:/docker-entrypoint-initdb.d -e TZ="Asia/Shanghai" -e MYSQL_DATABASE="order" -e MYSQL_ROOT_PASSWORD="123"  mysql --init-connect='SET NAMES utf8' --character-set-server=utf8 --collation-server=utf8_general_ci --explicit_defaults_for_timestamp=1
 
